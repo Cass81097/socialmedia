055acc57b4deed7319f620c8d2c16455cd3b91f8
Cass81097 <truonghoanganh.810@gmail.com>
2023-09-12 13:47:52 +0700

    edit css

--- FE/src/components/search/SearchResult.jsx
+++ FE/src/components/search/SearchResult.jsx
@@ -1,15 +1,36 @@
 import "../../styles/search-bar/SearchResult.css"
+import React, { useEffect, useState, useContext } from "react";
+import { baseUrl, getRequest, postRequest } from "../../utils/services"
+import { Link, useNavigate } from "react-router-dom";
 
 export const SearchResult = ({ result, userId }) => {
-  
-  
-  return (
-    <div
-      className="search-result"
-      onClick={(e) => alert(`You selected ${result}!`)}
-    > 
+  const navigate = useNavigate();
+  const [userSearched, setUserSearched] = useState([])
 
-      {result}
+  console.log(userSearched[0]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getRequest(`${baseUrl}/users/find/id/${userId}`);
+        setUserSearched(response);
+      } catch (error) {
+        console.error("Error checking friend status:", error);
+      }
+    };
+    fetchData();
+  }, [userId]);
+
+  const handleSearchUser = () => {
+    window.location.href = `/${userSearched[0].username}`;
+  };
+
+  return (
+    <div className="search-result" onClick={handleSearchUser}>
+      <div className="search-result-container">
+        <img src={userSearched[0]?.avatar} alt="" />
+        <p>{result}</p>
+      </div>
     </div>
   );
 };
